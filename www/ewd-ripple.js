b1676e0d7d6e3539857ef7b6da42fd81743b766e
robtweed <rtweed@mgateway.com>
2016-11-14 10:08:48 +0000

    Buil 1.18: Stability enhancements

--- www/ewd-ripple.js
+++ www/ewd-ripple.js
@@ -24,62 +24,59 @@
  |  limitations under the License.                                          |
  ----------------------------------------------------------------------------
 
-1 November 2016
+  17 October 2016
 
 */
 
-function startEwdRipple() {
+var ewdXpress = require('ewd-xpress').master;
 
-  function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0)==' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length,c.length);
-      }
-    }
-    return "";
+var config = {
+  managementPassword: 'keepThisSecret!',
+  serverName: 'RippleOSI EWD3 Server',
+  port: 3000,
+  poolSize: 4,
+  webServerRootPath: '/opt/tomcat/ripple',
+  database: {
+    type: 'gtm'
+  },
+  lockSession: false,
+  /*
+  resilientMode: {
+    keepPeriod: 600 // just keep the last 10 minutes
   }
+  */
+};
 
-  EWD.on('ewd-registered', function() {
-    // add EWD session token to cookie so it gets sent with every REST request
-    EWD.log = true;
+var routes = [
+  {
+    path: '/api',
+    module: 'ewd-ripple'
+  }
+];
 
-    // First check if there's an existing cookie EWD token  
-    //  If so, is it for an active session?  If so, leave it alone
-    //  If not, reset the cookie with the newly-created token and
-    //    repopulate the patient cache ready for subsequent REST requests
+var q = ewdXpress.start(config, routes);
 
-    var ewdToken = getCookie('ewd-token');
-    if (ewdToken !== '') {
-      EWD.send({
-        type: 'checkToken',
-        params: {
-          token: ewdToken
-        }
-      }, function(responseObj) {
-        // if it's a currently active sesssion, leave the cookie alone
-        // otherwise update it with the new one
-        if (responseObj.message.expired) {
-          EWD.setCookie();
-          EWD.send({
-            type: 'authenticate'
-          });
-        }
-      });
-    }
-    else {
-      // no existing cookie so create a new one with the latest token
-      EWD.setCookie();
-      EWD.send({
-        type: 'authenticate'
-      });
-    }
-  });
+var app = ewdXpress.intercept().app;
 
-  EWD.start('ewd-ripple', null, io);
+var pasConfig = {
+  openEHR: {
+    pasModule: 'mysqlPAS',
+    summaryHeadings: ['allergies', 'problems', 'medications', 'contacts', {name: 'transfers', value: true}]
+  }
+};
+
+var pas = process.argv[2] || 'openEHR' 
+q.userDefined['rippleUser'] = pasConfig[pas];
+
+function errorHandler (err, req, res, next) {
+  if (res.headersSent) {
+    return next(err)
+  }
+  res.status(500)
+  res.render('error', { error: err })
 }
+
+app.use(errorHandler);
+
+
+
