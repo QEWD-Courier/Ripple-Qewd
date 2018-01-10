070f4dddecd692f9830316703d44e9be9b695fca
robtweed <rtweed@mgateway.com>
2018-01-10 13:54:06 +0000

    Build 2.31.0: Various updates & bugfixes

--- data/loadPatients.js
+++ data/loadPatients.js
@@ -3,13 +3,13 @@
  ----------------------------------------------------------------------------
  | qewd-ripple: QEWD-based Middle Tier for Ripple OSI                       |
  |                                                                          |
- | Copyright (c) 2016-18 Ripple Foundation Community Interest Company       |
+ | Copyright (c) 2016-17 Ripple Foundation Community Interest Company       |
  | All rights reserved.                                                     |
  |                                                                          |
  | http://rippleosi.org                                                     |
  | Email: code.custodian@rippleosi.org                                      |
  |                                                                          |
- | Author: Will Weatherill                                                  |
+ | Author: Rob Tweed, M/Gateway Developments Ltd                            |
  |                                                                          |
  | Licensed under the Apache License, Version 2.0 (the "License");          |
  | you may not use this file except in compliance with the License.         |
@@ -24,36 +24,19 @@
  |  limitations under the License.                                          |
  ----------------------------------------------------------------------------
 
-  10 January 2018
+  20 November 2017
 
 */
 
+var data = require('./patients.json');
 
-var getDetail = require('./getDetail');
-
-function getDocumentDetail(args, finished) {
-
-  console.log('getDocumentDetail');
-
-  var cachedDocument = args.session.data.$(['patients', args.patientId, 'documents']);
-  var cachedDetail = cachedDocument.$(['detail', args.sourceId]);
-  var cachedSummary = cachedDocument.$(['summary', args.sourceId]);
-
-  if (cachedDetail.exists) {
-    var response = cachedDetail.getDocument(true);
-    response.documentType = cachedSummary.$('documentType').value;
-    response.sourceId = args.sourceId;
-    return finished(response);
+module.exports = function() {
+  var patients = this.db.use('RipplePatients');
+  if (patients.exists) return false;
+  patients.$('byId').setDocument(data);
+  for (var id in data) {
+    patients.$(['byName', data[id].name, id]).value = id;
   }
+  return true;
+};
 
-  getDetail(args, function(results) {
-    if (!results.error) {
-      cachedDetail.setDocument(results);
-    }
-    if (cachedSummary.exists) response.documentType = cachedSummary.$('documentType').value;
-    response.sourceId = args.sourceId;
-    return finished(results);
-  });
-}
-
-module.exports = getDocumentDetail;
